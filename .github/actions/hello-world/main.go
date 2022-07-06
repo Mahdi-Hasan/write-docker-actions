99fd862847266025ffcd49ecb4e69b811ddba4d9
Mahdi Hasan <43575088+Mahdi-Hasan@users.noreply.github.com>
2022-07-06 14:13:18 +0600

    Update main.go

--- .github/actions/hello-world/main.go
+++ .github/actions/hello-world/main.go
@@ -8,10 +8,11 @@ import(
 func main() {
     firstGreeting := os.Getenv("INPUT_FIRSTGREETING")
     secondGreeting := os.Getenv("INPUT_SECONDGREETING")
-    pathOfGreating := os.Getenv("INPUT_PathOfGreating")
+    pathOfGreating := os.Getenv("INPUT_THIRDGREETING")
     
     fmt.Println("Hello First" + firstGreeting)
-    fmt.Println("Path " + pathOfGreating)
+    fmt.Println("Path1 " + secondGreeting)
+    fmt.Println("Path2 " + pathOfGreating)
     
     if secondGreeting != "" {
     fmt.Println("Hello " + secondGreeting)
